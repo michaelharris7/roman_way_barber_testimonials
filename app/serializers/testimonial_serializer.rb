974a410e3d34fdb2bda1e317018beda8136decba
Michael Harris <michaeljh7@gmail.com>
2018-05-08 03:51:46 -0600

    Fixed custom_user showing for testimonials

--- app/serializers/testimonial_serializer.rb
+++ app/serializers/testimonial_serializer.rb
@@ -1,5 +1,5 @@
 class TestimonialSerializer < ActiveModel::Serializer
-  attributes :id, :content, :testimonial_user_id
+  attributes :id, :content, :testimonial_user_id, :custom_user
 
   belongs_to :testimonial_user
 end
