60be7056ef39be37133f6f77c449b6d18a27ed03
Michael Harris <michaeljh7@gmail.com>
2018-05-24 13:22:16 -0600

    Updated cors.rb with newer front-end url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,21 +8,10 @@
 # Rails.application.config.middleware.use Rack::Cors do
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://romanway-barber-frontend.herokuapp.com', 'http://localhost:4200'
+    origins 'https://evansbarberstylist.herokuapp.com/', 'https://romanway-barber-frontend.herokuapp.com', 'http://localhost:4200'
 
     resource '*',
     :headers => :any,
-    # :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
     :methods => [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
-
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins '*'  # Never do this on a live site. Only on development mode.
-
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
